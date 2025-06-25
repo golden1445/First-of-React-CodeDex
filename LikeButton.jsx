ea0bd5b0cd26fcc3a07d8d67f5b9fe215b9d757d
Golden Sharma <sharmagolden142@gmail.com>
2025-06-25 18:15:09 +0530

    Add files via upload

---
+++ LikeButton.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button>LikeButton</button>
+      <button>FollowButton</button>
+    </div>
+    
+  )
+}
+
